694fe3bc36ae196653b265e0d0fdf976c762afda
Kevin F. Garrity <kevin.garrity@nist.gov>
2021-05-27 23:38:05 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,3 +4,8 @@ push!(LOAD_PATH,"../src/")
 using Documenter, testdocs
 
 makedocs(sitename="My Documentation")
+
+deploydocs(;
+    repo="github.com/kfgarrity/testdocs.jl",
+    push_preview=true,
+)
